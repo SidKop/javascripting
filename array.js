7d30baa9c66a4a18acb34529027424aa033f720f
SidKop <tranterc@gmail.com>
2015-05-24 23:35:10 +0800

    array filter

---
+++ array.js
@@ -0,0 +1,8 @@
+function getShortMessages(messages) {
+      // SOLUTION GOES HERE
+      messages.filter(<50);
+    // return messages < 50;
+
+    }
+
+    module.exports = getShortMessages
--- map123.js
+++ map123.js
@@ -1,12 +1,17 @@
 
+    // function doubleAll(numbers) {
+    //   var result = []
+    //   for (var i = 0; i < numbers.length; i++) {
+    //     result.push(numbers[i] * 2)
+    //   }
+    //   return result
+    // }
+    //
+    // module.exports = doubleAll
 
 
-    function doubleAll(numbers) {
-      var result = []
-      for (var i = 0; i < numbers.length; i++) {
-        result.push(numbers[i] * 2)
-      }
-      return result
+    module.exports = function doubleAll(numbers) {
+      return numbers.map(function double(num) {
+        return num * 2
+      })
     }
-
-    module.exports = doubleAll
