85794af0498e040683bc519a5c9a2ad62a89d668
SidKop <tranterc@gmail.com>
2015-05-25 14:26:22 +0800

    last version

--- array.js
+++ array.js
@@ -1,8 +1,24 @@
 function getShortMessages(messages) {
-      // SOLUTION GOES HERE
-      messages.filter(<50);
-    // return messages < 50;
+  return messages.filter(function(item) {
+    return item.message.length < 50 }).map(function(item) {
+      return item.message;
+    });
+}
 
-    }
+module.exports = getShortMessages
 
-    module.exports = getShortMessages
+// testing//new
+
+// test
+// function getShortMessages(messages) {
+//   var filtered_messages = messages.filter(function(message) {
+//     if (message.message.length < 50){
+//      return message.message;
+//     }
+// });
+//   var short_messages = filtered_messages.map(function(message){
+//     return message.message;
+//   });
+//   return short_messages;
+// }
+//My solution
